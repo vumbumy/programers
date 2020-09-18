8381dccf67ccce7c41187774a44c58446d5174bb
HanBeom Seo <vumy@dmalt.co>
2020-09-18 20:46:33 +0900

    불량 사용자(https://programmers.co.kr/learn/courses/30/lessons/64064) - 작성 중

--- 64064.js
+++ 64064.js
@@ -1,37 +1,8 @@
-var regExList = []
-var n = 0;
-var m = 0;
-
-function recv(index, user_id) {
-    if(m === index){
-        console.log()
-        return 1
-    }
-
-    let re = regExList[index]
-
-    let count = 0;
-
-    for(let j = 0;j<n;j++){
-        if(re.test(user_id[j])){
-            console.log(re, user_id[j])
-            let temp = user_id[j]
-
-            user_id[j] = ""
-
-            count += recv(index + 1, user_id)
-
-            user_id[j] = temp
-        }
-    }
-
-    return count
-}
-
 function solution(user_id, banned_id) {
-    n = user_id.length
-    m = banned_id.length
+    let n = user_id.length
+    let m = banned_id.length
 
+    let answer = 1;
     for(let b of banned_id){
         let regExp = /[*]/gi
 
@@ -39,10 +10,21 @@ function solution(user_id, banned_id) {
 
         let re = new RegExp('^' + b + '$')
 
-        regExList.push(re)
+        let count = 0;
+        for(let j = 0; j < n; j++){
+            if(re.test(user_id[j])){
+                console.log(b, user_id[j])
+                user_id[j] = ""
+
+                count++;
+            }
+        }
+        console.log(b, count)
+        answer *= count
     }
 
-    return recv(0, user_id);
+    return answer
 }
 
 console.log(solution(["frodo", "fradi", "crodo", "abc123", "frodoc"], ["*rodo", "*rodo", "******"]))
+console.log(solution(["frodo", "drodo", "crodo", "abc123", "frodoc"], ["*rodo", "*rodo", "***do"]))
