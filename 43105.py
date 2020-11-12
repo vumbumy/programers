6f48ab6b89ba921e40da13489d164c1144fe88aa
HanBeom <vumbumy@outlook.kr>
2020-11-12 16:30:57 +0900

    정수 삼각형(https://programmers.co.kr/learn/courses/30/lessons/43105) - 해결

--- 43105.py
+++ 43105.py
@@ -1,27 +1,27 @@
-import queue
+rows = 0
+dp = []
+
+
+def dfs(index, row, triangle):
+    if row == rows:
+        return triangle[row][index]
+
+    if not dp[row][index] == -1:
+        return dp[row][index]
+
+    dp[row][index] = triangle[row][index]
+    dp[row][index] += max(dfs(index, row + 1, triangle), dfs(index + 1, row + 1, triangle))
+
+    return dp[row][index]
 
 
 def solution(triangle):
+    global rows, dp
     rows = len(triangle) - 1
 
-    answer = 0
+    dp = [[-1 for _ in t] for t in triangle]
 
-    q = queue.Queue()
-    q.put((0, 0, 0))
-
-    while not q.empty():
-        index, row, sum = q.get()
-
-        sum += triangle[row][index]
-
-        if row == rows:
-            answer = max(answer, sum)
-            continue
-
-        q.put((index, row + 1, sum))
-        q.put((index + 1, row + 1, sum))
-
-    return answer
+    return dfs(0, 0, triangle)
 
 
 print(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]))
